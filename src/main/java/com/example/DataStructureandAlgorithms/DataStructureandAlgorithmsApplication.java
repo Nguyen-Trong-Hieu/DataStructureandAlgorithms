fbc899c9c877fe77fd31328a5a67c17a4905ac20
NguyenTrongHieu <nguyentronghieu14032003@gmail.com>
2024-09-23 17:16:38 +0700

    first commit

---
+++ src/main/java/com/example/DataStructureandAlgorithms/DataStructureandAlgorithmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.DataStructureandAlgorithms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataStructureandAlgorithmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataStructureandAlgorithmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DataStructureandAlgorithms/DataStructureandAlgorithmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DataStructureandAlgorithms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataStructureandAlgorithmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
